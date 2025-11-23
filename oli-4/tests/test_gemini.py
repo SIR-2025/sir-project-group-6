4141ba8babaadcb9e6921a95d46769ddd43afbc4
mvrozend <73127864+mvrozend@users.noreply.github.com>
2025-11-23 16:48:58 +0100

    Delete oli-4/tests/test_gemini.py

--- oli-4/tests/test_gemini.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAQ340KnDzQrlFR-ylxs4dPuKqDRVneHio")
-
-model = genai.GenerativeModel("gemini-2.5-flash")
-
-response = model.generate_content("You are a horse that can talk. You are ordering a beer in a bar, but a monkey is the barman and he does not speak the same language as you.")
-print(response.text)
